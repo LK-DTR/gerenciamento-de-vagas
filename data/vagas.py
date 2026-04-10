b473f51f443f752d7412b0f7b7b353de53769265
Rafael Ribas de Lima <116221590+ribasmar@users.noreply.github.com>
2026-04-10 19:24:39 +0000

    Created: main.py and vagas.py

---
+++ data/vagas.py
@@ -0,0 +1,20 @@
+# Lista de dicionários para representar diferentes tipos de vagas
+lista_vagas = [
+    # Setor Norte - Comum
+    {"id": 1, "tipo": "Padrão", "setor": "Norte", "status": "livre", "preco": 20.00},
+    {"id": 2, "tipo": "Padrão", "setor": "Norte", "status": "ocupada", "preco": 20.00},
+    {"id": 3, "tipo": "Padrão", "setor": "Norte", "status": "livre", "preco": 20.00},
+    
+    # Setor Sul - VIP (Mais caras)
+    {"id": 4, "tipo": "VIP", "setor": "Sul", "status": "livre", "preco": 50.00},
+    {"id": 5, "tipo": "VIP", "setor": "Sul", "status": "livre", "preco": 50.00},
+    
+    # Vagas Especiais (PCD / Idoso)
+    {"id": 6, "tipo": "PCD", "setor": "Oeste", "status": "livre", "preco": 15.00},
+    {"id": 7, "tipo": "Idoso", "setor": "Oeste", "status": "livre", "preco": 15.00},
+    
+    # Setor Leste - Coberto
+    {"id": 8, "tipo": "Coberta", "setor": "Leste", "status": "ocupada", "preco": 30.00},
+    {"id": 9, "tipo": "Coberta", "setor": "Leste", "status": "livre", "preco": 30.00},
+    {"id": 10, "tipo": "Coberta", "setor": "Leste", "status": "livre", "preco": 30.00},
+]
---
+++ main.py
@@ -0,0 +1,47 @@
+from vagas import lista_vagas
+from modules.visualizar_vagas import exibir_vagas
+from modules.atualizar_vagas import reservar_vaga
+from modules.vagas_reservadas import listar_reservas
+
+def menu() :
+
+    print("\n--- SISTEMA DE RESERVA DE VAGAS ---")
+    print("1. Visualizar Vagas Disponíveis")
+    print("2. Reservar uma Vaga")
+    print("3. Ver Relatório de Reservas")
+    print("0. Sair")
+    return input("Escolha uma opção: ")
+
+
+def main():
+    while True:
+        opcao = menu()
+
+        if opcao == "1":
+            # Chama a função do módulo visualizar-vagas.py
+            print("\nQUADRO DE VAGAS:")
+            exibir_vagas(lista_vagas)
+
+        elif opcao == "2":
+            # Solicita o ID e usa a lógica do módulo atualizar-vagas.py
+            id_vaga = int(input("Digite o ID da vaga que deseja reservar: "))
+            sucesso = reservar_vaga(lista_vagas, id_vaga)
+            if sucesso:
+                print(f"Sucesso! Vaga {id_vaga} agora está reservada.")
+            else:
+                print("Erro: Vaga não encontrada ou já ocupada.")
+
+        elif opcao == "3":
+            # Chama a função do módulo vagas-reservadas.py
+            print("\nRELATÓRIO DE OCUPAÇÃO:")
+            listar_reservas(lista_vagas)
+
+        elif opcao == "0":
+            print("Encerrando o sistema...")
+            break
+        
+        else:
+            print("Opção inválida, tente novamente.")
+
+if __name__ == "__main__":
+    main()
